162286441445c5798e2e128cc2a63646db83f61e
Wagner Franco de Jesus <dev.wagner@gmail.com>
2020-07-06 22:51:56 -0300

    Criada e testada primeira requisição - Dominio Categoria

--- src/main/java/com/hexacode/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/hexacode/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.hexacode.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hexacode.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Teste para validar funcionamento REST";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
