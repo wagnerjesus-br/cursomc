2d42498c88c4afa61fdee5585b5591540453297c
Wagner Franco de Jesus <dev.wagner@gmail.com>
2020-07-08 23:10:33 -0300

    Criado o repository e service para a entidade Categoria

---
+++ src/main/java/com/hexacode/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.hexacode.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hexacode.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
