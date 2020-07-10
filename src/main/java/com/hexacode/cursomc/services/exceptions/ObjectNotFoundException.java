68a08c127efb0055e9c1a1723804cb70df533b02
Wagner Franco de Jesus <dev.wagner@gmail.com>
2020-07-10 00:47:05 -0300

    Tratamento de not_found utilizando padrão spring para Categoria

---
+++ src/main/java/com/hexacode/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.hexacode.cursomc.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public ObjectNotFoundException(String mensagem, Throwable cause) {
+		super(mensagem, cause);
+	}
+
+}
